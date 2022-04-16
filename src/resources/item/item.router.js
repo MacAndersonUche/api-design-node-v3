49b1d2081a86336d5e09969f0efc564309ef2fb7
MacAnderson Uche <macanderson.uche@productcaregroup.com>
2022-04-16 01:37:14 +0100

    stuff

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,18 @@
 import { Router } from 'express'
-
+const controller = (res, req) => {
+  res.send({ me: 'hello' })
+}
 const router = Router()
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
